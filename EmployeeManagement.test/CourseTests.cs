0642ce12dc6d4d742969490d0c1e49ed68bfe89f
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-30 08:54:47 +0200

    Incluyo tests boolean y string

---
+++ EmployeeManagement.test/CourseTests.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmployeeManagement.test
+{
+    public class CourseTests
+    {
+        [Fact]
+        public void CourseConstructor_ConstructCourse_IsNewMustBeTrue()
+        {
+            // Arrange
+
+            // Act
+            var course = new Course("Disaster Management 101");
+
+            // Assert
+            Assert.True(course.IsNew);
+        }
+    }
+}
---
+++ EmployeeManagement.test/EmployeeFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+
+namespace EmployeeManagement.test
+{
+    public class EmployeeFactoryTests
+    {
+        [Fact] // Lo marco con el atributo "Fact". Significa que es un unit test method
+        public void CreateEmployee_constructInternalEmployee_SalaryMustBe2500()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            Assert.Equal(2500, employee.Salary);
+            /* NOTA: pueda haber más de un assert en un test, pero lo suyo es que haya solo uno*/
+        }
+    }
+}
