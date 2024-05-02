1a91747474801a43bdc5ead50904d293b37b403c
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-05-02 09:00:48 +0200

    Primer mocking

---
+++ EmployeeManagement.test/MoqTests.cs
@@ -0,0 +1,34 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.Services.Test;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.test
+{
+    public class MoqTests
+    {
+        [Fact]
+        public void FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculated()
+        {
+            // Arrange
+            var employeeManagementTestDataRepository =
+              new EmployeeManagementTestDataRepository();
+            //var employeeFactory = new EmployeeFactory();
+            var employeeFactoryMock = new Mock<EmployeeFactory>();
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                employeeFactoryMock.Object);
+
+            // Act 
+            var employee = employeeService.FetchInternalEmployee(
+                Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb"));
+
+            // Assert  
+            Assert.Equal(400, employee.SuggestedBonus);
+        }
+    }
+}
